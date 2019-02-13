17bbd2f96580b378c57aa728817846c382b29f7f
Chanel Tattler <chanel@andchill.com.au>
2019-02-13 21:39:49 +1100

    Reformatting

--- models/bookings/average_daily_rate_per_booking.sql
+++ models/bookings/average_daily_rate_per_booking.sql
@@ -2,13 +2,16 @@
 
 -- Joins the calendar view with ba_bookings to determine the average daily rate. Note this model does not consider Airbnb data as we do not have total paid.
 
+WITH  ba_bookings AS (SELECT * FROM {{ref('ba_bookings')}}),
+      nights_occupied_per_booking AS (SELECT * FROM {{ref('nights_occupied_per_booking')}})
+
 SELECT  ba_bookings.ba_id_code,
         ba_bookings.ba_booking_id,
         ba_bookings.first_night,
         ba_bookings.last_night,
         ba_bookings.total_charges/nights_occupied_per_booking.nights_occupied AS average_daily_rate
-FROM {{ref('ba_bookings')}}
-LEFT JOIN {{ref('nights_occupied_per_booking')}}
+FROM ba_bookings
+LEFT JOIN nights_occupied_per_booking
   ON nights_occupied_per_booking.ba_booking_id = ba_bookings.ba_booking_id
 WHERE ba_bookings.booking_status = 'Confirmed'
   AND ba_bookings.last_night < current_date()
--- models/properties/str_ltr_comparison.sql
+++ models/properties/str_ltr_comparison.sql
@@ -1,19 +1,34 @@
 {{ config(materialized='view') }}
 
+WITH  property_master_list AS ( SELECT * FROM {{ref('property_master_list')}} ),
+      date_onboarded AS (SELECT * FROM {{ref('date_onboarded')}}),
+      date_offboarded AS (SELECT * FROM {{ref('date_onboarded')}}),
+      calendar_view AS (SELECT * FROM {{ref('calendar_view')}}),
+      ba_bookings AS (SELECT * FROM  {{ref('ba_bookings')}})
+
+
+
+
+
+
+
+
+
+
 SELECT  cv.day,
         pm.ba_id_code,
         ba.ba_booking_id,
         pm.long_term_rental_estimate_per_week/7 AS daily_ltr,
         IFNULL(adr.average_daily_rate, 0) AS daily_str,
         IFNULL(adr.average_daily_rate, 0) -  pm.long_term_rental_estimate_per_week/7 AS diff
-FROM {{ref('property_master_list')}} pm
-JOIN {{ref('date_onboarded')}} don
+FROM property_master_list pm
+JOIN date_onboarded don
   ON pm.ba_id_code = don.ba_id_code
-JOIN {{ref('date_offboarded')}} dof
+JOIN date_offboarded dof
   ON dof.ba_id_code = pm.ba_id_code
-JOIN {{ref('calendar_view')}} cv
-  ON cv.day >= don.date_onboarded AND cv.day <=  dof.date_offboarded OR cv.day <= CURRENT_DATE()
-LEFT JOIN {{ref('ba_bookings')}} ba
+JOIN calendar_view cv
+  ON cv.day >= don.date_onboarded AND cv.day <= dof.date_offboarded OR cv.day <= CURRENT_DATE()
+LEFT JOIN ba_bookings ba
   ON pm.ba_id_code = ba.ba_id_code
 
 -- Need to create a join between cv and average daily rate only when there is a booking on that date
