b12b073b95e6485dbe0f9ab6f2f20d95dad15eff
Chanel Tattler <chanel@andchill.com.au>
2019-03-04 14:45:13 +1100

    Fixing Hubspot deals issues.

--- models/base_models/hubspot_deals.sql
+++ models/base_models/hubspot_deals.sql
@@ -3,6 +3,8 @@
 -- Taking raw data from hubspot deals.
 
 SELECT  dealid AS deal_id,
+        CASE  WHEN properties.pipeline.value = '57bc66ca-756a-4adb-871e-4804f9b34b2c' THEN 'Client Pipeline'
+        ELSE  properties.pipeline.value END AS pipeline,
         properties.booking_automation_id.value AS ba_id_code,
         properties.property_address.`value` AS property_address,
         properties.property_state.value AS property_state,
@@ -23,14 +25,16 @@ SELECT  dealid AS deal_id,
         properties.referral_partner_email.value AS referral_partner_email,
         properties.hubspot_owner_id.sourceid AS hubspot_owner,
         properties.hubspot_owner_id.sourceid AS hubspot_owner_email,
-        properties.lead_bdm.value AS lead_bdm,
+        CASE WHEN properties.hubspot_owner_id.sourceid = bdm_id_code.bdm_email THEN CONCAT(bdm_id_code.bdm_first_name, " ", bdm_id_code.bdm_last_name) END AS lead_bdm,
         bdm_id_code.bdm_first_name AS bdm_first_name,
         bdm_id_code.bdm_last_name AS bdm_last_name,
         properties.dealstage.sourceid AS dealstage_id,
-        properties.dealstage.value AS deal_stage,
+        CASE  WHEN properties.dealstage.value = '124079' THEN 'property_listed'
+              ELSE properties.dealstage.value END AS deal_stage,
         CAST(properties.createdate.value AS DATE) AS deal_open_date,
-        CAST(properties.dealstage.`timestamp` AS DATE) AS date_of_deal,
+        EXTRACT(DATE FROM properties.dealstage.`timestamp`) AS date_of_deal,
         properties.closedate.value AS close_date
 FROM `and-chill-database.hubspot.deals`
 LEFT JOIN {{ref('bdm_id_code')}}
   ON bdm_id_code.bdm_id_code = `and-chill-database.hubspot.deals`.properties.lead_bdm.value
+GROUP BY 1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27,28,29,30
